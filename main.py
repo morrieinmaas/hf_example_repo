c8ee7c3a28604568209b1aadb20bbb2465f3c4d8
Moritz Schlichting <mo@traect.dev>
2025-11-05 17:20:34 +0100

    minimal starting point

---
+++ main.py
@@ -0,0 +1,15 @@
+# from subject import LMConfig
+# from agrabber import ActivationGrabber
+from subject import Subject
+
+
+if __name__ == "__main__":
+    print("Hello, world!")
+    # subject = Subject(LMConfig())
+    # sub_gen = subject.generate("Hello, world!")
+    # for token_id in sub_gen:
+    #     print(subject.decode(token_id), end="", flush=True)
+    # grabber = ActivationGrabber(subject)
+    # print(grabber.get_activations("Hello, world!"))
+        
+    
